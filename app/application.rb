5f395dca9695492c8edafed9c7ffd0c966ad5ab4
MisterC1ean <robert.keller22@gmail.com>
2020-03-29 23:14:56 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application
 
-    @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+   
     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
   
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
-        if item =@@items.find{|i| i.name == item_name}
+        if item = @@items.find{|i| i.name == item_name}
           resp.write item.price
         else 
           resp.status = 400
@@ -21,4 +21,3 @@ class Application
     end
   
   end
-  
